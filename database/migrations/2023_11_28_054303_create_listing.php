d94f1830697bbc9eb9eb72fe1d7f8a8570aafa6a
chhunly <60867999+chhunlyyy@users.noreply.github.com>
2023-11-28 12:51:17 +0700

    set up database and add listing migration

--- database/migrations/2023_11_28_054303_create_listing.php
+++ database/migrations/2023_11_28_054303_create_listing.php
@@ -13,15 +13,19 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('listing', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
             $table->timestamps();
+            $table->unsignedTinyInteger('beds');
+            $table->unsignedTinyInteger('baths');
+            $table->unsignedTinyInteger('area');
+
+            $table->tinyText('city');
+            $table->tinyText('code');
+            $table->tinyText('street');
+            $table->tinyText('street_nr');
+
+            $table->unsignedInteger('price');
         });
     }
 
@@ -32,6 +36,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('listing');
     }
 };
